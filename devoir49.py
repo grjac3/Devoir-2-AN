09c704bae837c97a3dd1d83bccd369dc4b31667b
charles trudeau <chtru70@ulaval.ca>
2025-02-21 10:39:12 -0500

    2eme

---
+++ devoir49.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pointfixe import pointfixe
+
+
+def fonc1(x):
+    y = (x+1)*(x-1)**2
+    return y
+r1 = -1
+r2 = 1
+
+#Question 1 point fixe
+def fonc_g1(fonc, x):
+    x - fonc1/5
+
+pointfixe()
